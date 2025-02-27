ac20ce0a6979ab243ffe2ebe33c35391f5bbe6e0
shobrook <shobrookj@gmail.com>
2025-02-27 18:32:44 -0500

    Added litellm-powered EmbeddingModel class

--- weightgain/EmbeddingModel.py
+++ weightgain/EmbeddingModel.py
@@ -1,14 +1,32 @@
-class EmbeddingModel(object):
-    def __init__(self, batch_size: int = 1):
-        self.adapter = None
-        self.batch_size = batch_size
+# Third party
+from litellm import embedding
 
-    @classmethod
-    def from_name(cls, name: str) -> "EmbeddingModel":
-        pass  # TODO: Use LiteLLM?
+# Local
+try:
+    from weightgain.Adapter import Adapter
+except ImportError:
+    from Adapter import Adapter
+
+
+class EmbeddingModel(object):
+    def __init__(self, model: str, batch_size: int = 2048, **kwargs):
+        self.model = model
+        self.batch_size = batch_size
+        self.kwargs = kwargs
+        self.adapter = None
 
     def set_adapter(self, adapter: Adapter):
         self.adapter = adapter
 
     def get_embeddings(self, inputs: list[str]) -> list[list[float]]:
-        pass  # TODO
+        response = embedding(self.model, inputs, **self.kwargs)
+
+        embeddings = []
+        for result in sorted(response["data"], key=lambda d: d["index"]):
+            vector = result["embedding"]
+            if self.adapter:
+                vector = vector @ self.adapter
+
+            embeddings.append(vector)
+
+        return embeddings
---
+++ weightgain/__init__.py
@@ -0,0 +1,8 @@
+try:
+    from weightgain.Adapter import Adapter
+    from weightgain.Dataset import Dataset
+    from weightgain.EmbeddingModel import EmbeddingModel
+except ImportError:
+    from Adapter import Adapter
+    from Dataset import Dataset
+    from EmbeddingModel import EmbeddingModel
